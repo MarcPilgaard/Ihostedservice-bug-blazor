deb4f4048e062861f9359a39569baaa4dff308ff
Marc Leander Pilgaard <Pilgaardmarc@gmail.com>
2024-10-23 20:09:25 +0200

    Initial commit

---
+++ BugExample/SomeRandomService.cs
@@ -0,0 +1,17 @@
+﻿namespace BugExample;
+
+public class SomeRandomService : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        while (true)
+        {
+            await Task.Delay(10000, cancellationToken);
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
